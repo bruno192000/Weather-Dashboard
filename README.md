d5cc2c105bd97b8d7ea236e1bd0091550f5cd505
Bruno Calderon <slardaromg@gmail.com>
2021-06-06 19:07:17 -0400

    adding index, css and js

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Weather-Dashboard
+## <h2>Weather Dashboard</h2>
+<p> Welcome back again, this time we have this weather dashboard that will inform you how the weather will be the present day and the 5 next.
+</p>
+ <li>
+            <a href="#Structure">Structure</a>
+             </li>
+            <li>
+                <a href="#Future">Future of weather dashboard</a>
+            </li>
+             <li>
+                 <a href="#contact">Contact</a>
+             </li>
+           
+
+## <h2 id="Structure"><i>Structure</i></h2>
+ 
+ <p> The weather dashboard contains, a searcher where you'll have to write the city. Once you do it then it will deploy the weather information such as: Temperature, Humidity, Wind speed,UV-index of the present day and the next 5 days. If you search and didn't write any city, then a alert will show up asking for the recurried info.
+ On the other hand, every city you search will be saved in a list below the searcher. Everytime you click in one of them, then it will show the weather info of the one selected.</p>
+
+ <img src="./Web capture_6-6-2021_184918_.jpeg" alt="Failed to load picture"></a>
+
+ ## <h2 id="Future"><i> Future of this weather dashboard</i></h2>
+ <p>Again, as you might see theres some mistakes that I plan to fix while i'm getting more experience. If you want to try it, just click <a href="https://bruno192000.github.io/Work-Day-scheduler/" target="_blank">here</p>
+
+ ## <h2 id="contact"><i>Contact information</i></h2>
+<p> As always, you'll find 3 ways to contact me:</p>
+    <li><a href="https://www.facebook.com/elrubiuhs" target="_blank">My facebook</a></li>
+    <li><a href="https://github.com/bruno192000" target="_blank">Github</a></li>
+    <li>My phone number: 6146959922</li>
---
+++ scrypt.js
@@ -0,0 +1,173 @@
+//var for the city that will be write
+var city="";
+// var for declare, using library with id
+var searchCity = $("#search-city");
+var searchButton = $("#search-button"); //searchbtn
+var clearButton = $("#clear-history");     //clearbtn
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var sCity=[];
+// a function to search the typed city and deploy it in uppercase
+function find(c){
+    for (var i=0; i<sCity.length; i++){
+        if(c.toUpperCase()===sCity[i]){
+            return -1;
+        }
+    }
+    return 1;
+}
+//the api key
+var APIKey="a0aca8a89948154a4182dcecc780b513";
+// to show the present weather of the city and also the next 5 days
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+    //if theres no content then it will show an alert
+    else{
+        alert("write a city please")
+    }
+
+}
+
+
+function currentWeather(city){
+    // the api url to get the info of weather
+    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    $.ajax({
+        url:queryURL,
+        method:"GET",
+    }).then(function(response){
+
+        
+        //icons from the api url
+        var weathericon= response.weather[0].icon;
+        var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
+        var date=new Date(response.dt*1000).toLocaleDateString();
+        
+        //parse the name of the city showed and mixed it with the date and icon
+        $(currentCity).html(response.name +"("+date+")" + "<img src="+iconurl+">");
+        // parse the response to display the current temperature.
+        // Convert the temp to fahrenheit
+
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        $(currentTemperature).html((tempF).toFixed(2)+"&#8457");
+        // Display the Humidity
+        $(currentHumidty).html(response.main.humidity+"%");
+        //Display Wind speed and convert to MPH
+        var ws=response.wind.speed;
+        var windsmph=(ws*2.237).toFixed(1);
+        $(currentWSpeed).html(windsmph+"MPH");
+       
+ // display uv index.
+        
+        UVIndex(response.coord.lon,response.coord.lat);
+        forecast(response.id);
+        if(response.cod==200){
+            sCity=JSON.parse(localStorage.getItem("cityname"));
+            
+            if (sCity==null){
+                sCity=[];
+                sCity.push(city.toUpperCase()
+                );
+                localStorage.setItem("cityname",JSON.stringify(sCity));
+                addToList(city);
+            }
+            else {
+                if(find(city)>0){
+                    sCity.push(city.toUpperCase());
+                    localStorage.setItem("cityname",JSON.stringify(sCity));
+                    addToList(city);
+                }
+            }
+        }
+    });
+}
+    // returning uv index response
+function UVIndex(ln,lt){
+    //var for uvindex and get lat, lon
+    var uvqURL="https://api.openweathermap.org/data/2.5/uvi?appid="+ APIKey+"&lat="+lt+"&lon="+ln;
+    $.ajax({
+            url:uvqURL,
+            method:"GET"
+            }).then(function(response){
+                $(currentUvindex).html(response.value);
+            });
+        }
+    
+// desplaying the next 5 days weather
+function forecast(cityid){
+    //using the url api for the next 5 days
+    var queryforcastURL="https://api.openweathermap.org/data/2.5/forecast?id="+cityid+"&appid="+APIKey;
+    $.ajax({
+        url:queryforcastURL,
+        method:"GET"
+    }).then(function(response){
+        // loop for the 5 days
+        for (i=0;i<5;i++){
+            var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+            var iconcode= response.list[((i+1)*8)-1].weather[0].icon;
+            // getting the icon from api url
+            var iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
+            var tempK= response.list[((i+1)*8)-1].main.temp;
+            var tempF=(((tempK-273.5)*1.80)+32).toFixed(2);
+            var humidity= response.list[((i+1)*8)-1].main.humidity;
+        
+            $("#fDate"+i).html(date);
+            $("#fImg"+i).html("<img src="+iconurl+">");
+            $("#fTemp"+i).html(tempF+"&#8457");
+            $("#fHumidity"+i).html(humidity+"%");
+        }
+        
+    });
+}
+
+//adding the searched citys to the list group. using lower case for the searched citys
+function addToList(c){
+    var listEl= $("<li>"+c.toLowerCase()+"</li>");
+    $(listEl).attr("class","list-group-item");
+    $(listEl).attr("data-value",c.toLowerCase());
+    $(".list-group").append(listEl);
+}
+// function to show the weather of every city in the list group once you click them
+function pastcitys(event){
+    var liEl=event.target;
+    if (event.target.matches("li")){
+        city=liEl.textContent.trim();
+        currentWeather(city);
+    }
+
+}
+
+// render function
+function loadlastCity(){
+    $("ul").empty();
+    var sCity = JSON.parse(localStorage.getItem("cityname"));
+    if(sCity!==null){
+        sCity=JSON.parse(localStorage.getItem("cityname"));
+        for(i=0; i<sCity.length;i++){
+            addToList(sCity[i]);
+        }
+        city=sCity[i-1];
+        currentWeather(city);
+    }
+
+}
+//function to clear all the citys from the list group
+function clearHistory(event){
+    event.preventDefault();
+    sCity=[];
+    localStorage.removeItem("cityname");
+    document.location.reload();
+
+}
+//click handlers
+$("#search-button").on("click",displayWeather);
+$(document).on("click",pastcitys);
+$(window).on("load",loadlastCity);
+$("#clear-history").on("click",clearHistory);
